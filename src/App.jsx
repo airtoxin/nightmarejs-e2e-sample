4d1efb0133efed4d1799111086b3c3037745d0d3
airtoxin <airtoxin@icloud.com>
2017-02-07 15:37:22 +0900

    Simple counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = { count: 0 };
+  }
+
   render() {
     return (
-      <h1>hello react!</h1>
+      <div>
+        <h1>Counts: {this.state.count}</h1>
+        <button
+          onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Increment
+        </button>
+        <button
+          onClick={() => this.setState({ count: this.state.count - 1 })}>
+          Decrement
+        </button>
+        <button
+          onClick={() => setTimeout(() => this.setState({ count: this.state.count + 1 }), 1000)}>
+          Increment async
+        </button>
+      </div>
     );
   }
 }
