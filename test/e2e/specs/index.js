99f01e57dcc534d4e359afc43bbd02afb9c7b754
airtoxin <airtoxin@icloud.com>
2017-02-08 00:06:40 +0900

    No need to use wait

--- test/e2e/specs/index.js
+++ test/e2e/specs/index.js
@@ -1,11 +1,17 @@
-import nightmare from 'nightmare';
 import assert from 'assert';
-import { goto, clickIncrement, clickDecrement, clickIncrementAsync, getCount, clickPlusN } from '../helpers';
+import getNightmareInstance, {
+  goto,
+  clickIncrement,
+  clickDecrement,
+  clickIncrementAsync,
+  getCount,
+  clickPlusN,
+} from '../helpers';
 
 describe('nightmarejs-e2e-sample', () => {
   let page;
   beforeEach(() => {
-    page = nightmare({ show: true });
+    page = getNightmareInstance();
   });
   afterEach(done => {
     page.halt(null, done);
@@ -49,21 +55,18 @@ describe('nightmarejs-e2e-sample', () => {
       )
   );
 
-  it('should increment +10, +300 and +575', () => page
-    .use(goto('/'))
-    .wait(1000)
-    .use(clickPlusN(10))
+it('should increment +10, +300 and +575', () => page
+  .use(goto('/'))
+  .use(clickPlusN(10))
+  .use(getCount())
+  .then(count => assert.strictEqual(count, 10))
+  .then(() => page
+    .use(clickPlusN(300))
     .use(getCount())
-    .then(count => assert.strictEqual(count, 10))
-    .then(() => page
-      .wait(1000)
-      .use(clickPlusN(300))
-      .use(getCount())
-      .then(count => assert.strictEqual(count, 10 + 300)))
-    .then(() => page
-      .wait(1000)
-      .use(clickPlusN(575))
-      .use(getCount())
-      .then(count => assert.strictEqual(count, 10 + 300 + 575)))
-  );
+    .then(count => assert.strictEqual(count, 10 + 300)))
+  .then(() => page
+    .use(clickPlusN(575))
+    .use(getCount())
+    .then(count => assert.strictEqual(count, 10 + 300 + 575)))
+);
 });
